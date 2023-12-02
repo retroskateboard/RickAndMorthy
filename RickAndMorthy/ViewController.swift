3f16f5276911ec0462a64292b78cbab989606031
Eric Rojas Pech <ericrokerz@gmail.com>
2023-12-01 19:31:42 -0600

    Initial Commit

---
+++ RickAndMorthy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RickAndMorthy
+//
+//  Created by Eric Rojas Pech on 01/12/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
